eaaec5fbada82c1a3382b3485dbaacb1e7b75c14
Charly <charlycarranza71@gmail.com>
2025-03-07 22:35:09 -0600

    Corrección de Firebase config con variables de entorno

--- index.js
+++ index.js
@@ -1,56 +1,50 @@
-const express = require('express');
-const admin = require('firebase-admin');
-const cors = require('cors');
-const bodyParser = require('body-parser');
-//const authRoutes = require('./routes/auth');
+const express = require("express");
+const admin = require("firebase-admin");
+const cors = require("cors");
+const bodyParser = require("body-parser");
 
-//const app = express();
-const port = 5000;
+const serviceAccount = {
+  type: process.env.TYPE,
+  project_id: process.env.PROJECT_ID,
+  private_key_id: process.env.PRIVATE_KEY_ID,
+  private_key: process.env.PRIVATE_KEY.replace(/\\n/g, "\n"), 
+  client_email: process.env.CLIENT_EMAIL,
+  client_id: process.env.CLIENT_ID,
+  auth_uri: process.env.AUTH_URI,
+  token_uri: process.env.TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.AUTH_PROVIDER_CERT_URL,
+  client_x509_cert_url: process.env.CLIENT_CERT_URL,
+  universe_domain: process.env.UNIVERSE_DOMAIN,
+};
 
-
-const serviceAccount = require('./firebaseConfig.json');
-
-
-if(!admin.apps.length){
-  
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
 });
-}else{
-  admin.app();
-}
 
 
-
-//const db = admin.firestore();
-const authRoutes = require('./routes/auth');
-const taskRoutes = require("./routes/tasks"); 
+const authRoutes = require("./routes/auth");
+const taskRoutes = require("./routes/tasks");
 const groupsRoutes = require("./routes/groups");
 const adminRoutes = require("./routes/auth");
 const usersRoutes = require("./routes/users");
 
-
 const app = express();
-
-
-
 
 app.use(cors());
 app.use(bodyParser.json());
 
-//app.use('/api/auth', authRoutes);
-
-
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
-app.use('/api/auth',authRoutes);
+app.use("/api/auth", authRoutes);
 app.use("/api", taskRoutes);
 app.use("/api/groups", groupsRoutes);
 app.use("/api", adminRoutes);
-app.use("/api", usersRoutes); 
+app.use("/api", usersRoutes);
 
-app.listen(port, () => {
-  console.log(`Server running on port ${port}`);
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
